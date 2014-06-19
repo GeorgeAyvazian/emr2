10a66dd9bc535b55906299989dd8d6acbcaf1735
Gavik Ayvazians <gayvazi@calstatela.edu>
2014-06-19 01:43:35 -0700

    knockout/viewmodel js moved to separate file. modals. form input styling and design

--- public/javascripts/myjs.js
+++ public/javascripts/myjs.js
@@ -1,16 +1,22 @@
 $('#dp3').datepicker();
 var myViewModel = {
     'firstName': ko.observable('x'),
+    'errorCheck': ko.observable('form-group'),
     'searchAll': function (data, event) {
         appRoutes.controllers.Application.findAllPatients(event.key).ajax({ 'success': function (data) {
             alert(data)
         } })
     },
-    'theTemplate': ko.observable('patient-center-template'),
-    'fetchTemplate': function (data, event) {
-        var templateToLoad = event.currentTarget.id;
-        this.theTemplate('addPatient' === templateToLoad ? 'patient-center-template' : 'email-center-template');
-    }//,
+//    'theTemplate': ko.observable('patient-center-template'),
+//    'fetchTemplate': function (data, event) {
+//        var templateToLoad = event.currentTarget.id;
+//        this.theTemplate('addPatient' === templateToLoad ? 'patient-center-template' : 'email-center-template');
+//    },
+    'highlight': function(data, event) {
+        var val = event.currentTarget.value;
+        if(!val || val.length < 1) window.console.log(this.errorCheck(this.errorCheck() + " has-error"));
+    }
+    //,
 //    'fetchSchedules': function () {
 //        $.getJSON('@routes.Schedules.view()',
 //            function (data) {
